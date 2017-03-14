87b8dc303201d0ef1e3c3a324e006148756a9ec9
Sven <glaglasven@live.com>
2017-03-14 15:36:10 +0100

    Basic gauge

--- Monitoring/MainPage.xaml.cs
+++ Monitoring/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Monitoring.Monitoring;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -25,6 +26,7 @@ namespace Monitoring
         public MainPage()
         {
             this.InitializeComponent();
+			Temperature a = new Temperature();
         }
     }
 }
---
+++ Monitoring/Monitoring/Temperature.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.System.Diagnostics;
+
+namespace Monitoring.Monitoring
+{
+	public class Temperature
+	{
+		public Temperature()
+		{
+			var a = ProcessDiagnosticInfo.GetForProcesses();
+			
+			foreach(var i in a)
+			{
+				string s = i.CpuUsage.GetReport().KernelTime.TotalMilliseconds.ToString();
+			
+				s += 1;
+			}
+
+		}
+	}
+}
